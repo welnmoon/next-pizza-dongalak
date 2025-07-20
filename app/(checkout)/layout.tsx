07ee5b8a0011974c3ef960cf58beaf53e33ed20b
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-07-20 13:46:38 +0500

    deploy

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -4,6 +4,7 @@ import "./global.css";
 import Header from "@/components/Header/Header";
 import Container from "@/components/Container";
 import { useSession } from "next-auth/react";
+import { Suspense } from "react";
 
 export default function CheckoutLayout({
   children,
@@ -15,12 +16,14 @@ export default function CheckoutLayout({
     <div className={`min-h-screen bg-[#F4F1EE]`}>
       <div className="">
         <Container>
-          <Header
-            session={session.data}
-            hasSearch={false}
-            hasCart={false}
-            className="border-b-1 border-gray-200 pb-4 pt-6 mb-6"
-          />
+          <Suspense fallback={null}>
+            <Header
+              session={session.data}
+              hasSearch={false}
+              hasCart={false}
+              className="border-b-1 border-gray-200 pb-4 pt-6 mb-6"
+            />
+          </Suspense>
           {children}
         </Container>
       </div>
