3c85554aff9b7357089344bc4b46ffa3e468dfae
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-07-24 11:36:01 +0500

    add vercel.json

--- prisma/migrations/20250628120244_rename_adress_to_address/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `adress` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "adress",
-ADD COLUMN     "address" TEXT;
